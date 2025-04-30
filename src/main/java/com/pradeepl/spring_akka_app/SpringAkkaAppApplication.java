1adbad664bd269cbd65c3c12252e050b404b4b22
pradeepl <pradeep.loganathan@hotmail.com>
2025-04-30 15:52:15 +1000

    initial commit

---
+++ src/main/java/com/pradeepl/spring_akka_app/SpringAkkaAppApplication.java
@@ -0,0 +1,13 @@
+package com.pradeepl.spring_akka_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAkkaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAkkaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pradeepl/spring_akka_app/SpringAkkaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeepl.spring_akka_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAkkaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
